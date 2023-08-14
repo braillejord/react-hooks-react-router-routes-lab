60789f6a6a85a404ab30524c8149b6312215e9af
BreElle Wells <breellejordyn@gmail.com>
2023-08-14 13:55:24 -0600

    render movies in movie component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink exact to="/movies">Movies</NavLink>
+      </li>
+      <li>
+        <NavLink exact to="/directors">Directors</NavLink>
+      </li>
+      <li>
+        <NavLink exact to="/actors">Actors</NavLink>
+      </li>
+    </ul >
+  )
 }
 
 export default NavBar;
